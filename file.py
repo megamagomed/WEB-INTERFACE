d34c7b7a1965b52dc84ca7197ab4e648c2f27377
megamagomed <34594010+megamagomed@users.noreply.github.com>
2022-06-19 19:58:11 +0300

    hren shto ponyatno

--- file.py
+++ file.py
@@ -1,1 +1,17 @@
-print('adsf')
+import requests
+import json
+
+url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
+
+headers = {
+	"accept": "application/json",
+	"X-RapidAPI-Key": "3f08ed03a7msh2bb88890105babcp1aeff4jsnb6cf585b978d",
+	"X-RapidAPI-Host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
+}
+
+response = json.loads(requests.request("GET", url, headers=headers).text)
+
+img_url = response['icon_url']
+with open("img_url.png", "wb") as file:
+    picture_response = requests.get(img_url)
+    file.write(picture_response.content)
